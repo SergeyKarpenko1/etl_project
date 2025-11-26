ed24b165f6dd5239eb19bfbc91e09f080c87e5e3
SergeyKarpenko1 <skarp35@gmail.com>
2025-11-26 18:10:26 +0300

    Changed main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import asyncio
 from service_llm_generator.workers.etl_pipeline import ETLPipeline
 from service_llm_generator.clients.client import DBCSVClient
+from service_llm_generator.utils.constants import RAW_DATA_PATH, PROCESSED_DATA_PATH
 
-INPUT = "data/raw/dirty_toxicity_data_large.csv"
-OUTPUT = "data/processed/clean_toxicity_data.csv"
 
 async def main():
-    db = DBCSVClient(input_path=INPUT, output_path=OUTPUT)
+    db = DBCSVClient(input_path=RAW_DATA_PATH, output_path=PROCESSED_DATA_PATH)
     pipeline = ETLPipeline(db_client=db)
-    await pipeline.run()
+    await pipeline.run(fetch_query=None, save_query=None)  # <- здесь
 
 if __name__ == "__main__":
     asyncio.run(main())
