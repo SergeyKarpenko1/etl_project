c58d65dd010bec61eafd86bb89b51bbf014a15af
SergeyKarpenko1 <skarp35@gmail.com>
2025-11-26 18:18:36 +0300

    Changes main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from service_llm_generator.utils.constants import RAW_DATA_PATH, PROCESSED_DATA_
 async def main():
     db = DBCSVClient(input_path=RAW_DATA_PATH, output_path=PROCESSED_DATA_PATH)
     pipeline = ETLPipeline(db_client=db)
-    await pipeline.run(fetch_query=None, save_query=None)  # <- здесь
+    await pipeline.run()  # просто так, без аргументов
 
 if __name__ == "__main__":
     asyncio.run(main())
