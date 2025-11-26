875131eb2a8ecbbb57ddd7a7fa9df2f7c0104a90
SergeyKarpenko1 <skarp35@gmail.com>
2025-11-26 17:53:07 +0300

    Change main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from etl!")
+import asyncio
+from service_llm_generator.workers.etl_pipeline import ETLPipeline
+from service_llm_generator.clients.client import DBCSVClient
 
+INPUT = "data/raw/dirty_toxicity_data_large.csv"
+OUTPUT = "data/processed/clean_toxicity_data.csv"
+
+async def main():
+    db = DBCSVClient(input_path=INPUT, output_path=OUTPUT)
+    pipeline = ETLPipeline(db_client=db)
+    await pipeline.run()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
