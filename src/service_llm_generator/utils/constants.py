21f18dda525d91e67f7824747745b0420b94695b
SergeyKarpenko1 <skarp35@gmail.com>
2025-11-26 18:07:43 +0300

    Added constants

---
+++ src/service_llm_generator/utils/constants.py
@@ -0,0 +1,6 @@
+# =========================
+# Пути к файлам
+# =========================
+RAW_DATA_PATH = "data/raw/dirty_toxicity_data_large.csv"
+PROCESSED_DATA_PATH = "data/processed/clean_toxicity_data.csv"
+LOGS_PATH = "logs/etl.log"
